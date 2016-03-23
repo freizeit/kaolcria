27b8bb5d7518a5b471d3638a8afe0ebc47ddca17
Muharem Hrnjadovic <mh@cloud1.it>
2016-03-23 21:23:27 +0100

    added tests for list_json_files()

--- lib/aircloak.ex
+++ lib/aircloak.ex
@@ -1,2 +1,54 @@
 defmodule Aircloak do
+  @moduledoc """
+  Aircloak coding exercise
+
+  Let's assume we have a dataset of users, and the city in which they live. We
+  want to discover how many users live in each city. The process would be the
+  following:
+
+  | User id | City      |
+  |---------|-----------|
+  | 1       | Berlin    |
+  | 2       | Berlin    |
+  | 3       | Berlin    |
+  | 4       | Berlin    |
+  | 5       | Berlin    |
+  | 6       | Berlin    |
+  | 7       | Zagreb    |
+  | 8       | Bucharest |
+  | 9       | Bonn      |
+  | 10      | K-town    |
+  | 11      | K-town    |
+
+  For each user you would report the tuple `(city, <CITY-NAME>)`, for example
+  `(city, Berlin)`, `(city, Zagreb)`, etc.
+
+  After the aggregation step, you would end up with:
+
+  | key  | value     | count |
+  |------|-----------|-------|
+  | city | Berlin    | 6     |
+  | city | Zagreb    | 1     |
+  | city | Bucharest | 1     |
+  | city | Bonn      | 1     |
+  | city | K-town    | 2     |
+
+  Which after the anonymization step ends up as
+
+  | key  | value     | count |
+  |------|-----------|-------|
+  | city | Berlin    | 6     |
+
+  In this case, the only information we could actually report to and end-user
+  would be that 6 people live in Berlin, and that there might, or might not, be
+  people living in other cities, but that we cannot really say whether that is
+  the case or not.
+  """
+
+
+  @doc """
+  Returns all the *.json files in the given `path`.
+  """
+  def list_json_files(path) do
+  end
 end
--- test/aircloak_test.exs
+++ test/aircloak_test.exs
@@ -6,3 +6,44 @@ defmodule AircloakTest do
     assert 1 + 1 == 2
   end
 end
+
+
+defmodule ListJsonFilesTest do
+  use ExUnit.Case
+  doctest Aircloak
+
+  setup context do
+    {tpath, 0} = System.cmd("mktemp", ["-d", "/tmp/air.XXXXX.cloak"])
+    tpath = String.rstrip(tpath)
+    Enum.each(context[:fs], fn f ->
+      path = tpath <> "/" <> f
+      File.touch!(path)
+    end)
+
+    on_exit fn ->
+      System.cmd("rm", ["-rf", tpath])
+    end
+
+    {:ok, tpath: tpath}
+  end
+
+
+  @tag fs: []
+  test "list_json_files(), empty dir", context do
+    assert Aircloak.list_json_files(context[:tpath]) == []
+  end
+
+
+  @tag fs: ["aa.abc", "bb.xxx"]
+  test "list_json_files(), no json files", context do
+    assert Aircloak.list_json_files(context[:tpath]) == []
+  end
+
+
+  @tag fs: ["ca.abc", "db.xxx", "ee.json", "ffjson", "gg.json"]
+  test "list_json_files(), 2 json files", context do
+    assert Aircloak.list_json_files(context[:tpath]) == ["ee.json", "gg.json"]
+  end
+
+
+end
